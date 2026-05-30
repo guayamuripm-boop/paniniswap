cab2f0bbcd7d8551e196aca7ce1f7d4df2def6ad
therockmejias <therockmejias@gmail.com>
2026-05-29 21:18:22 -0400

    fix redirect callback

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${window.location.origin}/album`
+        redirectTo: `${window.location.origin}/auth/callback`
       }
     })
   }
