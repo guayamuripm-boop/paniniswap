c6d25a912cff6cfbd4c3c4898dd89f690894ccd2
therockmejias <therockmejias@gmail.com>
2026-06-10 20:04:14 -0400

    Calendario page

---
+++ subir.bat
@@ -0,0 +1,6 @@
+Set-Location C:\Dev\metaxsports
+git add -A
+git commit -m "Calendario: pagina con 104 partidos, grupos y sedes"
+git push origin main
+Write-Host "Listo! Espera 2-3 min y entra a https://paniniswap-6hlt.vercel.app/calendario"
+Read-Host "Presiona Enter para salir"
